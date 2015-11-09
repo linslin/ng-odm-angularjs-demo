0f1d69c8c904b2ddb348f83388f3db7b486ffca8
Nils Gajsek <linslin@gmx.de>
2015-11-09 23:17:05 +0100

    Added content boxs on dashboard

---
+++ app/scripts/controllers/ContentGeneratorController.js
@@ -0,0 +1,16 @@
+/**
+ * Content generator controller
+ *
+ * @name        ContentGeneratorCtrl
+ * @author      Nils Gajsek <info@linslin.org>
+ * @copyright   Nils Gajsek http://www.linslin.org
+ * @package     ng-odm-angular.js-demo
+ * @version     1.0
+ */
+
+//ECMA6 Strict
+'use strict';
+
+angular.module('angularDemoApp')
+    .controller('ContentGeneratorCtrl', ['$odm', 'userModel',
+        function () {}]);
