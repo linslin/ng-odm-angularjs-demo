63cdf611b48cc887b258c96c303067abf0fd0b78
Nils Gajsek <linslin@gmx.de>
2015-11-10 12:20:08 +0100

    User list pagination added

--- app/scripts/controllers/Users/ListController.js
+++ app/scripts/controllers/Users/ListController.js
@@ -23,6 +23,57 @@ angular.module('angularDemoApp')
          */
         $scope.users = userModel.findAll();
 
+        /**
+         * Pagination item per page number
+         * @type {number}
+         */
+        var itemPerPage = 200;
+
+        /**
+         * Hold max count of pages at 50 items per pae.
+         * {Integer}
+         */
+        $scope.pageCount = Math.floor(userModel.findAll().length / itemPerPage) + 1;
+        /**
+         *  Current page id
+          * @type {number}
+         */
+        $scope.currentPage = 1;
+
+
+        /**
+         * Setup init state of current page user items
+         * @type {Array.<T>}
+         */
+        $scope.currentPageUserItem = $scope.users.slice((itemPerPage * ($scope.currentPage -1 )),itemPerPage *$scope.currentPage);
+
+
 
         // ###################################### scope control functions // ###########################################
-    }]);
+
+        /**
+         * Set page action
+         *
+         * @param {number} pageId
+         */
+        $scope.setPage = function (pageId) {
+            $scope.currentPage = pageId;
+            $scope.currentPageUserItem = $scope.users.slice((itemPerPage * ($scope.currentPage -1 )),itemPerPage *$scope.currentPage);
+        };
+    }])
+
+    /**
+     * Range filter
+     * @TODO maybe outsource
+     */
+    .filter('range', function() {
+        return function(input, total) {
+            total = parseInt(total);
+
+            for (var i=0; i<total; i++) {
+                input.push(i);
+            }
+
+            return input;
+        };
+    });
